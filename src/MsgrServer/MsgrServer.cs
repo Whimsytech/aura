f5d0d8c69052735bdb82612ae3559a150545669b
exec <exec@mabinoger.de>
2014-08-16 09:29:09 +0200

    Fixed compilation

--- src/MsgrServer/MsgrServer.cs
+++ src/MsgrServer/MsgrServer.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) Aura development team - Licensed under GNU GPL
 // For more information, see license file in the main folder
 
-using Aura.Msgr.Network;
 using Aura.Msgr.Network.Handlers;
 using Aura.Msgr.Util;
-using Aura.Shared.Network;
+using Aura.Shared;
 using Aura.Shared.Util;
 using System;
 
