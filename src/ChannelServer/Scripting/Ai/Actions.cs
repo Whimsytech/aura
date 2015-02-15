c95428c9d09aa2ade02c3bece83184d8d74dfe3d
Xcelled <j12@ketelaar.com>
2015-02-15 13:08:32 -0500

    Added basic types for AI system

---
+++ src/ChannelServer/Scripting/Ai/Actions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Aura.Channel.Scripting.Ai
+{
+	public abstract class AiAction : DependencyObject
+	{
+		public string Name { get; set; } // Optional name for e.g. jumping
+
+		public abstract void Execute();
+	}
+}
---
+++ src/ChannelServer/Scripting/Ai/Ai.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Aura.Channel.Scripting.Ai
+{
+	[ContentProperty("Actions")]
+	public class Ai
+	{
+		public string Name { get; set; }
+
+		public List<AiAction> Actions { get; set; }
+
+		public Ai()
+		{
+			Actions = new List<AiAction>();
+		}
+	}
+}
