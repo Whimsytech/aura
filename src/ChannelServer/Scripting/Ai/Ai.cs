b6c34ff44064db142ea8007295d92ff0564e0abf
Xcelled <j12@ketelaar.com>
2015-02-18 17:21:48 -0500

    Added Load method to Ai

--- src/ChannelServer/Scripting/Ai/Ai.cs
+++ src/ChannelServer/Scripting/Ai/Ai.cs
@@ -7,19 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Markup;
+using System.Xaml;
 
 namespace Aura.Channel.Scripting.Ai
 {
 	[ContentProperty("Actions")]
 	public class Ai
 	{
+		private static readonly XamlXmlReaderSettings _readerSettings = new XamlXmlReaderSettings { LocalAssembly = typeof(Ai).Assembly };
+
 		public string Name { get; set; }
+		public string BasedOn { get; set; }
 
 		public List<Element> Actions { get; set; }
 
 		public Ai()
 		{
 			Actions = new List<Element>();
+		}
+
+		public static Ai Load(string file)
+		{
+			var reader = new XamlXmlReader(file, _readerSettings);
+
+			var ai = (Ai)System.Windows.Markup.XamlReader.Load(reader);
+
+			if (!string.IsNullOrEmpty(ai.BasedOn))
+			{
+				var aiBase = Load(ai.BasedOn);
+
+				MergeAis(aiBase, ai);
+			}
+
+			return ai;
+		}
+
+		private static void MergeAis(Ai basedOn, Ai derived)
+		{
+			var newActions = new List<Element>(basedOn.Actions); // Copy base
+
+			var namedActions = newActions.Where(a => !string.IsNullOrEmpty(a.Name))
+				.ToDictionary(a => a.Name.ToUpperInvariant());
+			var derivedNamedActions = derived.Actions.Where(a => !string.IsNullOrEmpty(a.Name))
+				.ToDictionary(a => a.Name.ToUpperInvariant())
+				.Where(n => namedActions.ContainsKey(n.Key));
+
+			// Any elements that are re-specified in derived need to be overwritten
+			foreach (var n in derivedNamedActions)
+			{
+				namedActions[n.Key] = n.Value;
+			}
+
+			// Add any new actions from derived
+			newActions.AddRange(derived.Actions.Except(derivedNamedActions.Select(a => a.Value)));
 		}
 	}
 }
